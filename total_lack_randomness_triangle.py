0e5a1be9160ed06774adda5143efeced4d6ad5fb
Elie Wolfe <elupus@gmail.com>
2025-06-12 19:50:02 -0400

    New code for tripartite lack of randomness in the triangle scenario. (RGB3 gives infeasible, meaning maybe yes tripartite randomness.)

---
+++ total_lack_randomness_triangle.py
@@ -0,0 +1,163 @@
+import gurobipy as gp
+from gurobipy import GRB
+from itertools import repeat
+import numpy as np
+
+time_limit = GRB.INFINITY
+tol = 1e-5
+return_dist = True
+print_model = True
+
+
+def check_total_lack_randomness(p_ABC: np.ndarray, cardX: int) -> bool:
+    (cardA, cardB, cardC) = p_ABC.shape
+    rL = tuple(range(cardX))
+
+    with (gp.Env(empty=True) as env):
+        env.start()
+        with gp.Model("qcp", env=env) as m:
+            m.params.NonConvex = 2
+            m.setParam('FeasibilityTol', tol)
+            m.setParam('OptimalityTol', 0.01)
+            m.setParam('TimeLimit', time_limit)
+            m.setParam('Presolve', 0)
+            m.setParam('PreSparsify', 1)
+            m.setParam('PreQLinearize', 1)
+            m.setParam('PreDepRow', 1)
+            m.setParam('Symmetry', 2)
+            m.setParam('Heuristics', 1.0)
+            m.setParam('RINS', 0)
+            m.setParam('MIPFocus', 1)
+            m.setParam('MinRelNodes', 0)
+            m.setParam('ZeroObjNodes', 0)
+            m.setParam('ImproveStartGap', 0)
+            m.setParam('PartitionPlace', 31)
+
+            # Define fully unpacked probabilities
+            unpacked_C_cardinalities = tuple(repeat(cardC, times=cardX))
+            unpacked_all_cardinalities = (cardA, cardB) + unpacked_C_cardinalities + (cardX,)  # this corresponds to ABC0C1...X
+
+            # Introduce core MVars, Q_ABCC and Q_L, from which everything else is derived.
+            Q_ABCCX = m.addMVar(unpacked_all_cardinalities, lb=0, ub=1, name="Q_ABCCX")
+            for x in range(cardX):
+                m.addConstr(Q_ABCCX[..., x].sum() <= 1, name="Unpacked Normalization") #Really?
+            Q_X = m.addMVar((cardX,), lb=0, ub=1, name="Q_X")
+            m.addConstr(Q_X.sum() == 1, name="Normalization of hidden L")
+
+            # NS constraint
+            Q_BCCX = Q_ABCCX.sum(axis=0)
+            m.addConstr(Q_BCCX[..., :1] == Q_BCCX[..., 1:], name="NS constraint")
+
+            # Introduce Q_ACCX, Q_CC, Q_AX to capture nonlinear constraint
+            Q_ACCX = Q_ABCCX.sum(axis=1)
+            # m.addConstr(Q_ACCX == Q_ABCCX.sum(axis=1), name="Q_ACCX from Q_ABCCX")
+            Q_AX = m.addMVar((cardA, cardX), lb=0, ub=1, name="Q_AX")
+            Q_CC = m.addMVar(unpacked_C_cardinalities, lb=0, ub=1, name="Q_CC")
+            axes_of_CC = tuple(range(1, cardX + 1))
+            m.addConstr(Q_AX == Q_ACCX.sum(axis=axes_of_CC), name="Q_AX from Q_ACCX")
+            axes_of_A = 0
+            m.addConstr(Q_CC == Q_ACCX.sum(axis=axes_of_A)[..., 0], name="Q_CC from Q_ACCX")
+
+            # Nonlinear constraint: Q_ACCX = Q_AX * Q_CC
+            Q_AX_reshaped_for_multiplication = Q_AX.reshape((cardA,) + tuple(repeat(1, times=(cardX))) + (cardX,))
+            Q_CC_reshaped_for_multiplication = Q_CC.reshape((1,) + Q_CC.shape + (1,))
+
+            m.addConstr(Q_ACCX == Q_AX_reshaped_for_multiplication * Q_CC_reshaped_for_multiplication,
+                        name="Q_ACCX = Q_AX * Q_CC")
+
+            # Linear constraint: P_ABC relates to Q_LABCC
+            temp = gp.MQuadExpr.zeros(p_ABC.shape)
+            default_axes_to_trace_out = set(range(cardX + 3))
+            default_axes_to_trace_out.discard(cardX + 2)  # removing axis for X
+            default_axes_to_trace_out.discard(0)  # removing axis for A
+            default_axes_to_trace_out.discard(1)  # removing axis for B
+            for l in rL:
+                axes_to_trace_out = tuple(sorted(default_axes_to_trace_out.difference({l + 2})))
+                temp += Q_X[l] * Q_ABCCX[..., l].sum(axis=axes_to_trace_out)
+            m.addConstr(p_ABC == temp, name="P_ABC from Q_LAABCC")
+
+            cardY = cardC ** cardX
+            cardEa = cardA
+            cardSx = cardX
+            cardEb = cardB
+            cardSy = cardY
+            cardE = cardEa * cardEb
+            cardS = cardSx * cardSy
+
+            cardinalities_R = (cardA, cardB, cardE, cardX, cardY, cardS)
+            R_ABEabXYSxy = m.addMVar(cardinalities_R, lb=0, ub=1, name="R_ABEaEbXYSxSy")
+            for settings in np.ndindex(cardX, cardY, cardS):
+                m.addConstr(R_ABEabXYSxy[..., settings].sum() <= 1, name="Normalization")  # Needed?
+
+            # No-signaling constraints for R
+            R_ABXYSxSy = R_ABEabXYSxy.sum(axis=2)
+            m.addConstr(R_ABXYSxSy[:, :, :, :, :1] == R_ABXYSxSy[:, :, :, :, 1:], name="NS for S")
+            R_AEXYS = R_ABEabXYSxy.sum(axis=1)
+            m.addConstr(R_AEXYS[:, :, :, :1, :] == R_AEXYS[:, :, :, 1:, :], name="NS for Y")
+            R_BEXYS = R_ABEabXYSxy.sum(axis=0)
+            m.addConstr(R_BEXYS[:, :, :1, :, :] == R_BEXYS[:, :, 1:, :, :], name="NS for X")
+
+            # Conditional of Q for compatibility constraint
+            R_ABYX = m.addMVar((cardA, cardB, cardY, cardX), lb=0, ub=1, name="R_ABYX")  # Weird order on purpose
+            for (x, y) in np.ndindex(cardX, cardY):
+                m.addConstr(R_ABXYSxSy[:, :, x, y, 0] == R_ABYX[:, :, y, x], name="R_ABYX transposed R_ABYX")
+            Q_CC_reshaped_for_conditioning = Q_CC.reshape((1, 1, cardY, 1))
+            Q_ABCCX_reshaped_for_conditioning = Q_ABCCX.reshape(R_ABYX.shape)
+            m.addConstr(Q_ABCCX_reshaped_for_conditioning == R_ABYX * Q_CC_reshaped_for_conditioning)
+
+            # need to finish this, one constraint is missing
+            R_ABEabXYSxy_reshaped_for_perfect_prediction = R_ABEabXYSxy.reshape((cardA, cardB, cardEa, cardEb,
+                                                                                 cardX, cardY, cardSx, cardSy))
+            for (b, eb, y) in np.ndindex(cardB, cardEb, cardY):
+                if not b == eb:
+                    m.addConstr(R_ABEabXYSxy_reshaped_for_perfect_prediction[:, b, :, eb, :, y, :, y] == 0, name="Perfect prediction of Bob")
+            for (a, ea, x) in np.ndindex(cardA, cardEa, cardX):
+                if not a == ea:
+                    m.addConstr(R_ABEabXYSxy_reshaped_for_perfect_prediction[a, :, ea, :, x, :, x, :] == 0, name="Perfect prediction of Alice")
+
+            m.setObjective(0.0, GRB.MAXIMIZE)
+            m.Params.NonConvex = 2
+
+            m.update()
+            current_status = m.getAttr("Status")
+            gurobi_status_codes = [
+                'LOADED',
+                'OPTIMAL',
+                'INFEASIBLE',
+                'INF_OR_UNBD',
+                'UNBOUNDED',
+                'CUTOFF',
+                'ITERATION_LIMIT',
+                'NODE_LIMIT',
+                'TIME_LIMIT',
+                'SOLUTION_LIMIT',
+                'INTERRUPTED',
+                'NUMERIC',
+                'SUBOPTIMAL',
+                'INPROGRESS',
+                'USER_OBJ_LIMIT',
+                'WORK_LIMIT']
+            if current_status == 1:
+                m.optimize()
+                current_status = m.getAttr("Status")
+                current_status_string = gurobi_status_codes[current_status - 1]
+                print(f"Status: {current_status_string}")
+            if m.getAttr("SolCount"):
+                record_to_preserve = dict()
+                for var in m.getVars():
+                    record_to_preserve[var.VarName] = var.X
+                    if print_model:
+                        print(var.VarName, " := ", var.X)
+
+            m.dispose()
+        env.dispose()
+    return current_status_string
+
+
+if __name__ == "__main__":
+    from probabilities import prob_RGB3
+
+    RGB3_specific = prob_RGB3(1 / np.sqrt(2), 0.95)
+    print("Normalization check:", RGB3_specific.sum())
+
+    print(check_total_lack_randomness(RGB3_specific, cardX=2))
