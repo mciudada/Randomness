9160b3c59cb7325d2893c4e0e8e7bc63f411e12c
Maria Ciudad Alañón <144954963+mciudada@users.noreply.github.com>
2025-06-27 19:08:00 +0200

    Update optimal_bound_RGB3.py

--- optimal_bound_RGB3.py
+++ optimal_bound_RGB3.py
@@ -62,46 +62,46 @@ def optimal_nonclassical_bound_RGB3(cardL: int)-> bool:
                 else:
                     m.addConstr(p_ABC[i,j,k]==0)
             
-            # # # # Introduce core MVars, Q_ABCC and Q_L, from which everything else is derived.
-            # # # Q_ABCCX = m.addMVar(unpacked_all_cardinalities, lb=0, name="Q_ABCCX")
-            # # # for x in np.ndindex(cardL):
-            # # #     m.addConstr(Q_ABCCX[...,x].sum() <= 1, name="Unpacked Normalization")
-            # # # Q_L = m.addMVar((cardL,), lb=0, name="Q_L")
-            # # # m.addConstr(Q_L.sum() == 1, name="Normalization of hidden L")
+            # Introduce core MVars, Q_ABCC and Q_L, from which everything else is derived.
+            Q_ABCCX = m.addMVar(unpacked_all_cardinalities, lb=0, name="Q_ABCCX")
+            for x in np.ndindex(cardL):
+                m.addConstr(Q_ABCCX[...,x].sum() <= 1, name="Unpacked Normalization")
+            Q_L = m.addMVar((cardL,), lb=0, name="Q_L")
+            m.addConstr(Q_L.sum() == 1, name="Normalization of hidden L")
 
-            # # # # NS constraint
-            # # # Q_BCCX = m.addMVar((cardB,)+unpacked_C_cardinalities+(cardL,), lb=0, name="Q_BCCX")
-            # # # m.addConstr(Q_BCCX == Q_ABCCX.sum(axis=0), name="Q_BCCX from Q_ABCCX")
-            # # # for x in tuple(range(cardL-1)):
-            # # #     m.addConstr(Q_BCCX[...,x] == Q_BCCX[...,x+1], name="NS constraint")
+            # NS constraint
+            Q_BCCX = m.addMVar((cardB,)+unpacked_C_cardinalities+(cardL,), lb=0, name="Q_BCCX")
+            m.addConstr(Q_BCCX == Q_ABCCX.sum(axis=0), name="Q_BCCX from Q_ABCCX")
+            for x in tuple(range(cardL-1)):
+                m.addConstr(Q_BCCX[...,x] == Q_BCCX[...,x+1], name="NS constraint")
             
-            # # # # Introduce Q_ACC, Q_CC, Q_AX to capture nonlinear constraint
-            # # # Q_ACCX = m.addMVar((cardA,) + unpacked_C_cardinalities + (cardL,), name="Q_ACCZ")
-            # # # m.addConstr(Q_ACCX == Q_ABCCX.sum(axis=1), name="Q_ACCX from Q_ABCCX")
-            # # # Q_AX = m.addMVar((cardA,cardL), name="Q_AX")
-            # # # Q_CC = m.addMVar(unpacked_C_cardinalities, name="Q_CC")
-            # # # axes_of_CC = tuple(range(1,cardL+1))
-            # # # m.addConstr(Q_AX == Q_ACCX.sum(axis=axes_of_CC), name="Q_AX from Q_ACCX")
-            # # # axes_of_A = 0
-            # # # m.addConstr(Q_CC == Q_ACCX.sum(axis=axes_of_A)[...,0], name="Q_CC from Q_ACCX")
+            # Introduce Q_ACC, Q_CC, Q_AX to capture nonlinear constraint
+            Q_ACCX = m.addMVar((cardA,) + unpacked_C_cardinalities + (cardL,), name="Q_ACCZ")
+            m.addConstr(Q_ACCX == Q_ABCCX.sum(axis=1), name="Q_ACCX from Q_ABCCX")
+            Q_AX = m.addMVar((cardA,cardL), name="Q_AX")
+            Q_CC = m.addMVar(unpacked_C_cardinalities, name="Q_CC")
+            axes_of_CC = tuple(range(1,cardL+1))
+            m.addConstr(Q_AX == Q_ACCX.sum(axis=axes_of_CC), name="Q_AX from Q_ACCX")
+            axes_of_A = 0
+            m.addConstr(Q_CC == Q_ACCX.sum(axis=axes_of_A)[...,0], name="Q_CC from Q_ACCX")
             
-            # # # # Nonlinear constraint: Q_ACCX = Q_AX * Q_CC
-            # # # Q_AX_reshaped_for_multiplication = Q_AX.reshape((cardA,) + tuple(repeat(1, times=(cardL))) + (cardL,))
-            # # # Q_CC_reshaped_for_multiplication = Q_CC.reshape((1,) + Q_CC.shape + (1,))
+            # Nonlinear constraint: Q_ACCX = Q_AX * Q_CC
+            Q_AX_reshaped_for_multiplication = Q_AX.reshape((cardA,) + tuple(repeat(1, times=(cardL))) + (cardL,))
+            Q_CC_reshaped_for_multiplication = Q_CC.reshape((1,) + Q_CC.shape + (1,))
             
-            # # # m.addConstr(Q_ACCX == Q_AX_reshaped_for_multiplication * Q_CC_reshaped_for_multiplication, name="Q_ACCX = Q_AX * Q_CC")
+            m.addConstr(Q_ACCX == Q_AX_reshaped_for_multiplication * Q_CC_reshaped_for_multiplication, name="Q_ACCX = Q_AX * Q_CC")
 
-            # # # # Linear constraint: P_ABC relates to Q_LABCC 
-            # # # # P_ABC = m.addMVar((cardA, cardB, cardC), lb=0, name="P_ABC")
-            # # # temp = gp.MQuadExpr.zeros(p_ABC.shape)
-            # # # default_axes_to_trace_out = set(range(cardL + 3))
-            # # # default_axes_to_trace_out.discard(cardL+2) # removing axis for X
-            # # # default_axes_to_trace_out.discard(0) # removing axis for A
-            # # # default_axes_to_trace_out.discard(1) # removing axis for B
-            # # # for l in rL:
-            # # #     axes_to_trace_out = tuple(sorted(default_axes_to_trace_out.difference({l+2})))
-            # # #     temp += Q_L[l] * Q_ABCCX[...,l].sum(axis=axes_to_trace_out)
-            # # # m.addConstr(p_ABC == temp, name="P_ABC from Q_LAABCC")
+            # Linear constraint: P_ABC relates to Q_LABCC 
+            # P_ABC = m.addMVar((cardA, cardB, cardC), lb=0, name="P_ABC")
+            temp = gp.MQuadExpr.zeros(p_ABC.shape)
+            default_axes_to_trace_out = set(range(cardL + 3))
+            default_axes_to_trace_out.discard(cardL+2) # removing axis for X
+            default_axes_to_trace_out.discard(0) # removing axis for A
+            default_axes_to_trace_out.discard(1) # removing axis for B
+            for l in rL:
+                axes_to_trace_out = tuple(sorted(default_axes_to_trace_out.difference({l+2})))
+                temp += Q_L[l] * Q_ABCCX[...,l].sum(axis=axes_to_trace_out)
+            m.addConstr(p_ABC == temp, name="P_ABC from Q_LAABCC")
 
             ineq = m.addVar(lb = -1e20, name="ineq")
             m.addConstr(ineq ==3*((lambda_1**3)*(u**2)*v-(lambda_0**3)*u*(v**2))**2 - 3*(u**2)*((lambda_1**6)+(lambda_0**6)) + 2*((lambda_0**6)+((lambda_1**3)*(u**3)+(lambda_0**3)*(v**3))**2) + (lambda_1**6) + ((lambda_1**3)*(v**3)-(lambda_0**3)*(u**3))**2, name="objective")
