f3aba19e6ab312804e5c51a1082e558fa971f269
Elie Wolfe <elupus@gmail.com>
2025-07-30 11:09:06 -0400

    Introduce a few auxiliary variables in an attempt to slightly speed up computation

--- optimal_bound_RGB3.py
+++ optimal_bound_RGB3.py
@@ -16,32 +16,61 @@ def optimal_nonclassical_bound_RGB3(cardL: int, print_model=True, stop_if_violat
             lambda_1 = m.addVar(lb=0, ub=1, name="lambda_1")
             u = m.addVar(lb=0, ub=1, name="u")
             v = m.addVar(lb=0, ub=1, name="v")
-            m.addConstr(u ** 2 + v ** 2 == 1, name="Normalization parameters 1")
-            m.addConstr(lambda_0 ** 2 + lambda_1 ** 2 == 1, name="Normalization parameters 2")
+
+            # To accelerate computation, we define squares, cubes, and fourth powers
+            lambda_0__2 = m.addVar(lb=0, ub=1, name="lambda_0^2")
+            m.addConstr(lambda_0__2 == lambda_0 ** 2)
+            lambda_0__3 = m.addVar(lb=0, ub=1, name="lambda_0^3")
+            m.addConstr(lambda_0__3 == lambda_0 * lambda_0__2)
+            lambda_0__4 = m.addVar(lb=0, ub=1, name="lambda_0^4")
+            m.addConstr(lambda_0__4 == lambda_0__2**2)
+            lambda_0__6 = m.addVar(lb=0, ub=1, name="lambda_0^6")
+            m.addConstr(lambda_0__6 == lambda_0__2 * lambda_0__4)
+
+            lambda_1__2 = m.addVar(lb=0, ub=1, name="lambda_1^2")
+            m.addConstr(lambda_1__2 == lambda_1 ** 2)
+            lambda_1__3 = m.addVar(lb=0, ub=1, name="lambda_1^3")
+            m.addConstr(lambda_1__3 == lambda_1 * lambda_1__2)
+            lambda_1__4 = m.addVar(lb=0, ub=1, name="lambda_1^4")
+            m.addConstr(lambda_1__4 == lambda_1__2 ** 2)
+            lambda_1__6 = m.addVar(lb=0, ub=1, name="lambda_1^6")
+            m.addConstr(lambda_1__6 == lambda_1__2 * lambda_1__4)
+
+            u__2 = m.addVar(lb=0, ub=1, name="u^2")
+            m.addConstr(u__2 == u ** 2)
+            u__3 = m.addVar(lb=0, ub=1, name="u^3")
+            m.addConstr(u__3 == u * u__2)
+            v__2 = m.addVar(lb=0, ub=1, name="v^2")
+            m.addConstr(v__2 == v ** 2)
+            v__3 = m.addVar(lb=0, ub=1, name="v^3")
+            m.addConstr(v__3 == v * u__2)
+
+            m.addConstr(u__2 + v__2 == 1, name="Normalization parameters 1")
+            m.addConstr(lambda_0__2 + lambda_1__2 == 1, name="Normalization parameters 2")
 
             # Define RGB3 distribution in terms of parameters
             p_ABC = m.addMVar((3, 3, 3), lb=0, name="p_ABC")
             for (i, j, k) in np.ndindex(3, 3, 3):
                 if (i, j, k) == (0, 0, 1) or (i, j, k) == (0, 1, 0) or (i, j, k) == (1, 0, 0):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 4) * (lambda_0 ** 2) * (u ** 2) + (lambda_0 ** 4) * (
-                                lambda_1 ** 2) * (v ** 2))
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__4) * (lambda_0__2) * (u__2) + (lambda_0__4) * (
+                                lambda_1__2) * (v__2))
                 elif (i, j, k) == (0, 0, 2) or (i, j, k) == (0, 2, 0) or (i, j, k) == (2, 0, 0):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 4) * (lambda_0 ** 2) * (v ** 2) + (lambda_0 ** 4) * (
-                                lambda_1 ** 2) * (u ** 2))
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__4) * (lambda_0__2) * (v__2) + (lambda_0__4) * (
+                                lambda_1__2) * (u__2))
                 elif (i, j, k) == (1, 1, 2) or (i, j, k) == (1, 2, 1) or (i, j, k) == (2, 1, 1):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 3 * u ** 2 * v - lambda_0 ** 3 * v ** 2 * u) ** 2)
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__3 * u__2 * v - lambda_0__3 * v__2 * u) ** 2)
                 elif (i, j, k) == (1, 2, 2) or (i, j, k) == (2, 2, 1) or (i, j, k) == (2, 1, 2):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 3 * u * v ** 2 + lambda_0 ** 3 * v * u ** 2) ** 2)
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__3 * u * v__2 + lambda_0__3 * v * u__2) ** 2)
                 elif (i, j, k) == (1, 1, 1):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 3 * u ** 3 + lambda_0 ** 3 * v ** 3) ** 2)
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__3 * u__3 + lambda_0__3 * v__3) ** 2)
                 elif (i, j, k) == (2, 2, 2):
-                    m.addConstr(p_ABC[i, j, k] == (lambda_1 ** 3 * v ** 3 - lambda_0 ** 3 * u ** 3) ** 2)
+                    m.addConstr(p_ABC[i, j, k] == (lambda_1__3 * v__3 - lambda_0__3 * u__3) ** 2)
                 else:
                     m.addConstr(p_ABC[i, j, k] == 0)
 
             # Define objective: a polynomial function, which, when negative, certifies nonclassicality.
             ineq = m.addVar(lb = -10, ub=10, name="ineq")
-            m.addConstr(ineq ==3*((lambda_1**3)*(u**2)*v-(lambda_0**3)*u*(v**2))**2 - 3*(u**2)*((lambda_1**6)+(lambda_0**6)) + 2*((lambda_0**6)+((lambda_1**3)*(u**3)+(lambda_0**3)*(v**3))**2) + (lambda_1**6) + ((lambda_1**3)*(v**3)-(lambda_0**3)*(u**3))**2, name="objective")
+            m.addConstr(ineq ==3*((lambda_1__3)*(u__2)*v-(lambda_0__3)*u*(v__2))**2 - 3*(u__2)*((lambda_1__6)+(lambda_0__6)) + 2*((lambda_0__6)+((lambda_1__3)*(u__3)+(lambda_0__3)*(v__3))**2) + (lambda_1__6) + ((lambda_1__3)*(v__3)-(lambda_0__3)*(u__3))**2, name="objective")
             m.setObjective(ineq, GRB.MINIMIZE)
             m.setParam('BestObjStop', -stop_if_violation)
 
